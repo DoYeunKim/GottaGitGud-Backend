cbdfef9ef68ba80c9ec3b16b0fcd48e41bd8c7de
Do Yeun Kim <doyeunkim.vm@gmail.com>
2021-01-27 10:20:58 -0500

    initialized Spring Boot backend

---
+++ src/main/java/com/revature/gottagitgud/GottaGitGudApplication.java
@@ -0,0 +1,13 @@
+package com.revature.gottagitgud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GottaGitGudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GottaGitGudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/revature/gottagitgud/GottaGitGudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.gottagitgud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GottaGitGudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
